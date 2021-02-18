f0414d27d771523f8400b0141b2bcb207faf1f2c
vitto2 <47305804+vitto2@users.noreply.github.com>
2021-02-18 09:28:54 -0300

    Utilizando numeros com virgulas
    Alteração para inserir o replace, efetuando troca da vírgula para o ponto

--- backendcalc.js
+++ backendcalc.js
@@ -1,11 +1,16 @@
 
+
 var calc = () => {  
 
 var peso = document.getElementById("in1").value; 
+peso = parseFloat(peso.replace(",","."));
+
 
 var altura = document.getElementById("in2").value; 
+altura = parseFloat(altura.replace(",","."));
 
 var imc = peso / (altura * altura); 
+imc = parseFloat(imc);
 
 var classificacao; 
 
@@ -15,21 +20,24 @@ if (imc <= 18.5) {
     
 } else if  (imc > 18.5 && imc <=24.9) { 
     
-    classificacao = "Valor ideal";
+    classificacao = "Peso ideal";
 
-} else if (imc>=25 && imc <= 29.9) { 
+} else if (imc>=25.00 && imc <= 29.9) { 
 
     classificacao = "Sobrepeso"; 
 
-} else if (imc>=30 && imc <= 34.9) { 
+} else if (imc>=30.00 && imc <= 34.9) { 
 
     classificacao = "Obesidade Grau I";
-} else if (imc>=35 && imc<=39.9) { 
+
+} else if (imc>=35.00 && imc<=39.9) {
+
     classificacao = "Obesidade Grau II"
-} else { 
+
+} else if(imc>=40.00) { 
     classificacao = "Obesidade Grau III"
 }
 
-document.getElementById("numcalc").innerHTML = classificacao + " - " + imc.toFixed(2)
+document.getElementById("numcalc").innerHTML = imc.toFixed(2) + " -\n"+ classificacao
 
 }
